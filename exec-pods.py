3a3a03343f4076f9895d68c5ce6e8c1c606a77a6
kareem <kareem.elsayed@fly365.com>
2019-06-06 01:01:37 +0200

    Create exec commands on pods script

---
+++ exec-pods.py
@@ -0,0 +1,23 @@
+from kubernetes import config
+from kubernetes.client import Configuration
+from kubernetes.client.apis import core_v1_api
+from kubernetes.stream import stream
+
+config.load_kube_config()
+c = Configuration()
+c.assert_hostname = False
+Configuration.set_default(c)
+api = core_v1_api.CoreV1Api()
+name = input('Set pod name: ')
+namespace = input('Set namespace: ')
+cmd = input('set command: ')
+resp = api.read_namespaced_pod(name=name,namespace=namespace)
+exec_command = [
+    '/bin/sh',
+    '-c',
+    cmd]
+resp = stream(api.connect_get_namespaced_pod_exec, name, namespace,
+              command=exec_command,
+              stderr=True, stdin=False,
+              stdout=True, tty=False)
+print("Response: " + resp)
