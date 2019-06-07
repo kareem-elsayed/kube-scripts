0150c3f856ae8e2081a58909a3c02eabf062aa13
kareem <kareem.elsayed@fly365.com>
2019-06-07 19:57:13 +0200

    Refactor-03

--- exec-pods.py
+++ exec-pods.py
@@ -1,12 +1,8 @@
 from kubernetes import config
-from kubernetes.client import Configuration
 from kubernetes.client.apis import core_v1_api
 from kubernetes.stream import stream
 
 config.load_kube_config()
-conf = Configuration()
-conf.assert_hostname = False
-Configuration.set_default(conf)
 api = core_v1_api.CoreV1Api()
 name = input('Set pod name: ')
 namespace = input('Set namespace: ')
